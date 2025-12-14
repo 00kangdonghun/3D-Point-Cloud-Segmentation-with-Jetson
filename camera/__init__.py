304a6f2ff1eb713b5474ff30b2fdc2e3172f1e28
Donghun Kang <112920967+00kangdonghun@users.noreply.github.com>
2025-12-15 00:31:30 +0900

    Enable auto_connect_camera function
    Uncommented the auto_connect_camera function to enable camera connection functionality.

--- camera/__init__.py
+++ camera/__init__.py
@@ -7,6 +7,19 @@ from .base_camera import Camera
 
 import pyrealsense2.pyrealsense2 as rs
 
+# '''
+# def auto_connect_camera() -> Camera:
+#     context = rs.context()
+
+#     for device in context.query_devices():
+#         if device.get_info(rs.camera_info.name) == "Intel RealSense L515":
+#             serial = device.get_info(rs.camera_info.serial_number)
+#             return RealsenseCamera(serial, serial)
+
+#     return MockRealsenseCamera("mock", Dataset(
+#         Path("__file__").parent / "data" / "mock", only_annotated=False
+#     ))
+# '''
 def auto_connect_camera() -> Camera:
     context = rs.context()
 
